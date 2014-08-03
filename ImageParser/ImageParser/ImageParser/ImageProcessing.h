5a5a2cd18e03680297092362d4c3c8b293162c1c
tlau9195@gmail.com <tlau9195@gmail.com>
2014-08-02 17:55:37 -0700

    image processing file added

---
+++ ImageParser/ImageParser/ImageParser/ImageProcessing.h
@@ -0,0 +1,14 @@
+//
+//  ImageProcessing.h
+//  ImageParser
+//
+//  Created by Tianyi Zhang on 02/08/2014.
+//  Copyright (c) 2014 TimLau. All rights reserved.
+//
+
+#ifndef __ImageParser__ImageProcessing__
+#define __ImageParser__ImageProcessing__
+
+#include <iostream>
+
+#endif /* defined(__ImageParser__ImageProcessing__) */
